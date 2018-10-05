d72d75008a240c1f9ba3336e9f26f9eef64c5766
Hifive6 <39879994+Hifive6@users.noreply.github.com>
2018-10-05 19:57:04 -0400

    adding more info to controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,33 +16,36 @@ router.get("/", function(req, res) {
   });
 });
 
-router.post("/api/burger", function(req, res) {
+router.post("/", function(req, res) {
   burger.create([
     "burger_name", "devoured"
   ], [
     req.body.burger_name, req.body.devoured
-  ], function(result) {
+  ], function() {
     // Send back the ID of the new quote
-    res.json({ id: result.insertId });
+    res.json("/");
   });
 });
 
-router.put("/api/burger/:id", function(req, res) {
+router.put("/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
 
   burger.update({
     devoured: req.body.devoured
-  }, condition, function(result) {
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
+  }, condition, function() {
+    res.redirect("/") 
   });
 });
+
+router.delete("/:id", function(req, res){
+    var condition = "id = " + req.params.id;
+
+    burger.delete(condition, function(){
+        res.redirect("/");
+    })
+})
 
 // Export routes for server.js to use.
 module.exports = router;
