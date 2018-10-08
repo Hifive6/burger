0fb11ac080a57843fc43994f99f79d730f41c716
Hifive6 <39879994+Hifive6@users.noreply.github.com>
2018-10-08 18:07:45 -0400

    fixing html

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -16,7 +16,7 @@ router.get("/", function(req, res) {
   });
 });
 
-router.post("/", function(req, res) {
+router.post("/api/burgers", function(req, res) {
   burger.insertOne([
     "burger_name", "devoured"
   ], [
@@ -27,7 +27,7 @@ router.post("/", function(req, res) {
   });
 });
 
-router.put("/:id", function(req, res) {
+router.put("/api/burgers/:id", function(req, res) {
   var condition = "id = " + req.params.id;
 
   console.log("condition", condition);
---
+++ public/assets/js/addburger.js
@@ -0,0 +1,43 @@
+$(function(){
+    $(".eaten").on("click", function(event){
+        const id = $(this).data("id");
+
+        const newDevouredBurger = {
+            devoured: true
+        };
+        console.log("call to api" + id);
+        console.log("newly eaten" + newDevouredBurger.devoured);
+
+        $.ajax("/api/burgers" + id,
+        {
+            type: "PUT",
+            data: newDevouredBurger
+        }).then(
+            function(){
+                console.log("changed" + id + id + "to", newDevouredBurger);
+                location.reload();
+            }
+        )
+    })
+
+
+
+$(".insert-burger").on("submit", function(event){
+    event.preventDefault();
+
+    const newAddBurger = {
+        name: $("#addburger").val().trim(),
+        devoured: false
+    };
+    
+    $.ajax("/api/burgers", {
+        type: "POST",
+        data: newAddBurger
+    }).then(
+        function(){
+        console.log("created new burger" + newAddBurger);
+        location.reload();
+    }
+    )
+})
+})
